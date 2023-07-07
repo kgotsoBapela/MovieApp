f34e1e087c358b2b3c50822ac2c64efb77b61276
Kgotso Bapela <kgotso.bapela@gmail.com>
2023-07-07 23:37:06 +0200

    Completed JS with all functions required

--- script.js
+++ script.js
@@ -3,21 +3,61 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=a2b34b35851454418bf12c1f7e728cd7&query="';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
 // Get initial movies
 getMovies(API_URL);
 
+// showMovies(getMovies);
 async function getMovies(url) {
     const response = await fetch(url)
     const data = await response.json();
 
-    console.log(data.results);
-    return data.results;
+    showMovies(data.results);
   }
+  
+function showMovies(movies){
+    main.innerHTML = '';
 
-  form.addEventListener('submit', (e)=> {
+    movies.forEach((movie)=>{
+        const{title, poster_path, vote_average, overview} = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+        
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+        
+            <div class="movie-info">
+                <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+
+            <div class="overview">
+                <h3>overview</h3>
+                ${overview}
+            </div>
+        
+        `
+        main.append(movieEl)
+    })
+}
+function getClassByRate(vote){
+    if(vote >= 8){
+        return 'green';
+    }
+    else if(vote >= 5){
+        return 'orange';
+    }
+    else{
+        return 'red';
+    }
+}
+
+form.addEventListener('submit', (e)=> {
     e.preventDefault();
 
     const searchTerm = search.value;
@@ -29,4 +69,4 @@ async function getMovies(url) {
     else{
         window.location.reload();
     }
-  })
+})
